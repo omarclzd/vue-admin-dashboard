7996c8bc8def6bf59ab01b3dbece47e4b739b419
omar <omasrt@gmail.com>
2019-08-09 14:58:05 -0500

    adds login routes and components

---
+++ src/views/SignInFlow/Signin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="login"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn"
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+}
+
+.login {
+  height: 50px;
+  width: 50px;
+  background: red;
+}
+</style>
