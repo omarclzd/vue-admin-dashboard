8e1fcde924c23e575528a57d2b818ea98b13ac3b
omar <omasrt@gmail.com>
2019-08-09 16:03:46 -0500

    adds signin form

--- src/views/SignInFlow/Signin.vue
+++ src/views/SignInFlow/Signin.vue
@@ -1,27 +1,103 @@
 <template>
   <div class="container">
-    <div class="login"></div>
+    <div class="request">
+      Don't have an account?
+      <router-link to="/request">Request an account</router-link>
+    </div>
+    <div class="login">
+      <img src="@/assets/logo.png" />
+      <h4>Sign in</h4>
+      <input />
+      <input />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Sign In</button>
+      <router-link to="/recover">Forgot your password?</router-link>
+      <button @click="toggleDarkMode">Toggle</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SignIn"
+  name: "SignIn",
+  data: {
+    isDarkMode: true
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
-
   min-height: 100vh;
 }
 
 .login {
-  height: 50px;
-  width: 50px;
-  background: red;
+  width: 400px;
+}
+
+.request {
+  position: absolute;
+  top: 40px;
+  right: 40px;
+  color: rgba(255, 255, 255, 0.3);
+
+  a {
+    color: white;
+  }
+}
+
+h4 {
+  margin: 0;
+  line-height: 34px;
+  font-size: 24px;
+  text-align: center;
+  color: #ffffff;
+}
+
+input {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  padding-left: 20px;
+  margin-top: 20px;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+
+button {
+  background: #56ccf2;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  border: none;
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 40px;
+}
+
+a {
+  line-height: 25px;
+  font-size: 16px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.3);
+  text-decoration: none;
 }
 </style>
