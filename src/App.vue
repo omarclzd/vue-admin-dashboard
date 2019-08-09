ea9d8cd95bde5e1f92c13ba9f49a80156c8ea2d2
omar <omasrt@gmail.com>
2019-08-09 17:04:15 -0500

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
 
 <style lang="scss">
 @import "@/global-styles/colors.scss";
